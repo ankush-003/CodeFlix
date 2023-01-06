6007914f18133e4b0d6482bb507a351ce716fdeb
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-01-06 20:21:41 +0530

    created user routes

---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const {loginUser, signupUser} = require('../controllers/userController')
+// login Route
+router.post('/login', loginUser)
+
+// signup Route
+router.post('/signup', signupUser)
+
+
+module.exports = router
