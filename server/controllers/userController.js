48cbfa48fab9fd95a6777bdfc30b17811defd8cf
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-01-14 21:44:14 +0530

    creating login and signup tokens

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,26 @@
 // User model
 const User = require('../models/userModel')
+// JSON Web Token
+const jwt = require('jsonwebtoken');
 
+//create and assign a token
+const createToken = (_id) =>{
+    return jwt.sign({_id}, process.env.TOKEN_SECRET, {
+        expiresIn: '1d'
+    })
+}
 // login user
 const loginUser = async (req, res) => {
-    res.json({'msg': 'login user',
-    'user': req.body
-})}
+    const {email, password} = req.body;
+    try {
+        const user = await User.login(email, password);
+        // create token
+        const token = createToken(user._id);
+        res.status(200).json({email, token});
+    } catch(error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 // signup user
 
@@ -13,7 +28,9 @@ const signupUser = async (req, res) => {
     const {email, password} = req.body;
     try {
         const user = await User.signup(email, password);
-        res.status(201).json({user});
+        // create token
+        const token = createToken(user._id);
+        res.status(201).json({email, token});
     } catch(error) {
         res.status(400).json({error: error.message});
     }
